86f185a0c704613cb7f34c647c99a72b8cb2449c
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-02 14:06:33 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+// --------
+// FUNZIONI
+// --------
+
+function myCreateElement(htmlElement,className,htmlValue){
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.classList.add('cell-'+htmlValue);
+    element.innerText = htmlValue;
+
+    element.addEventListener('click', function(){
+        alert(htmlValue);
+    })
+
+    return element;
+}
+
+function myAppendElement(containerElement, htmlElement){
+    containerElement.append(htmlElement);
+}
+
+// ----
+// MAIN
+// ----
+let cellNumber = 100;
+const containerBoard = document.querySelector('.board');
+
+for(let i = 1; i<= cellNumber; i++){
+    const createdElement = myCreateElement('div','cell', i);
+    myAppendElement(containerBoard, createdElement);
+}
